a8030d51ecf9caa52aa076c29d73c57646f2378f
maria <ca_t_u@hotmail.com>
2022-12-06 17:32:25 +0000

    initial commit

---
+++ myapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from myapp.models import *
+
+# Register your models here.
+admin.site.register(User)
+admin.site.register(Genre)
+admin.site.register(Music)
+admin.site.register(Comment)
+admin.site.register(Post)
---
+++ myapp/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+class User(models.Model):
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=9, unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    birth_date = models.DateField()
+    user_email = models.CharField(max_length=40, unique=True)
+    created_at = models.DateField(auto_now=True, editable=False)
+
+    def __str__(self):
+        return self.username
+
+class Genre(models.Model):
+    genre_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    genre_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.genre_name
+
+class Music(models.Model):
+    music_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    music_name = models.CharField(max_length=100)
+    artist_name = models.CharField(max_length=100)
+    album_name = models.CharField(max_length=100)
+    last_reccomended = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.music_name
+
+# Forum
+
+class Post(models.Model):
+    post_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    post_title = models.CharField(max_length=200)
+    post_text = models.TextField()
+    created_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.post_title
+
+    # foreign key what_is_about(genero, artista, musica), e do user
+
+class Comment(models.Model):
+    comment_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    comment_text = models.TextField
+    created_at = models.DateField(auto_now=True)
+
+    # foreign key do post e do user
+
+
+
+
